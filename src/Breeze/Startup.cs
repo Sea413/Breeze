2797f13b849ca727e620d63995e3ec7577477410
sea413 <seanmp42@gmail.com>
2016-04-22 11:01:30 -0700

    Create a foreign key between comments and games.

--- src/Breeze/Startup.cs
+++ src/Breeze/Startup.cs
@@ -35,6 +35,7 @@ namespace Breeze
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
             app.UseIISPlatformHandler();
             app.UseMvc(routes =>
             {
