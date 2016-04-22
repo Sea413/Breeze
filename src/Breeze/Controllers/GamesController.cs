4b93aa9deb57975ed6282449f0f34ead99b4ec47
sea413 <seanmp42@gmail.com>
2016-04-22 10:39:52 -0700

    Finish adding the final CRUD logic.

--- src/Breeze/Controllers/GamesController.cs
+++ src/Breeze/Controllers/GamesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Breeze.Models;
+using Microsoft.Data.Entity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -20,6 +21,39 @@ namespace Breeze.Controllers
         {
             var thisGame = db.Games.FirstOrDefault(games => games.GameId == id);
             return View(thisGame);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game game)
+        {
+            db.Games.Add(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisGame = db.Games.FirstOrDefault(games => games.GameId == id);
+            return View(thisGame);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Game game)
+        {
+            db.Entry(game).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult Delete(int id)
+        {
+            var thisGame = db.Games.FirstOrDefault(x => x.GameId == id);
+            db.Games.Remove(thisGame);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
